533bf23828dbeaff1199382e3d13e23f1deb98f4
vayts <aleksandrtarasovi44@gmail.com>
2023-02-16 02:38:02 +0200

    add visual for PartialPaymentModal.tsx component

--- client/src/components/Modal/PartialPaymentModal/PartialPaymentModal.tsx
+++ client/src/components/Modal/PartialPaymentModal/PartialPaymentModal.tsx
@@ -1,22 +1,60 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { PartialPaymentWrapper } from '@src/components/Modal/PartialPaymentModal/style';
 import { IPartialPayment } from '@src/components/Modal/PartialPaymentModal/types';
 import { Title } from '@src/components/Title/Title';
 import { useTranslation } from 'react-i18next';
 import { Input } from '@src/components/UI/Input/Input';
+import { Description } from '@src/components/Description/Description';
+import { Button } from '@src/components/UI/Button/Button';
 
 export const PartialPaymentModal: React.FC<IPartialPayment> = ({ debt }) => {
-	const [value, setValue] = useState(debt.value);
+	const [value, setValue] = useState('');
+	const paymentRef = useRef(null);
 	const { t } = useTranslation();
+	
+	useEffect(() => {
+		if (paymentRef.current) {
+			paymentRef.current.focus();
+		}
+	}, []);
 	
 	const onChangeHandler = (e) => {
 		setValue(e.target.value);
 	};
 	
+	const onSubmit = () => {
+	
+	};
+	
 	return (
 		<PartialPaymentWrapper>
-			<Title>{t('partialPaymentMenu')}</Title>
-			<Input name='partialPayment' id='partialPaymentNumber' onChange={onChangeHandler} value={value} type='number'/>
+			<Title margin='0 auto'>{t('partialPaymentMenu')}</Title>
+			<Description
+				margin='5px 0 20px'
+			>
+				{t('specifyAmountOfMoney')}
+			</Description>
+			<form>
+				<Input
+					refValue={paymentRef}
+					name='partialPayment'
+					id='partialPaymentNumber'
+					onChange={onChangeHandler}
+					value={value}
+					type='number'
+					placeholder={`${t('amountOfMoney')}: 1 ₴ \u2013 ${debt.value - 1} ₴`}
+					width='100%'
+					label={t('amountOfMoney')}
+					min={1}
+					max={debt.value - 1}
+				/>
+				<Button
+					onClick={onSubmit}
+					title={t('submit')}
+					margin='30px auto 0'
+					height='30px'
+				/>
+			</form>
 		</PartialPaymentWrapper>
 	);
 };
--- client/src/components/Modal/PartialPaymentModal/style.ts
+++ client/src/components/Modal/PartialPaymentModal/style.ts
@@ -2,4 +2,10 @@ import styled from 'styled-components';
 
 export const PartialPaymentWrapper = styled.div`
   padding: 0 25px 15px;
+	width: 450px;
+	max-width: 450px;
+	
+	& > h3 {
+		text-align: center;
+	}
 `;
